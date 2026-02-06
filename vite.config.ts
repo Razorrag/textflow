58c99bc576624533d7739cb89f97046fd1c2f268
anuragsingh-ship-i <anurag.singh@hexobyte.com>
2026-02-06 20:54:51 +0530

    try-add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
     return {
@@ -12,8 +14,8 @@ export default defineConfig(({ mode }) => {
       css: {
         postcss: {
           plugins: [
-            require('tailwindcss'),
-            require('autoprefixer'),
+            tailwindcss,
+            autoprefixer,
           ],
         },
       },
